6322b902d53a9ff38b1c6f6d098b17cd9b851c53
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2010-09-28 21:39:01 -0600

    I actually want INFO log level.

--- dbg.go
+++ dbg.go
@@ -20,7 +20,7 @@ const (
 	ERROR
 )
 		
-var CurrentLogLevel LogLevel = ERROR
+var CurrentLogLevel LogLevel = INFO
 var LogDefault func(v...interface{}) = log.Stdout
 var LogDefaultf func(fmt string, v...interface{}) = log.Stdoutf
 
