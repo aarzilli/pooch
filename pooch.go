6dac41240110dafcf36fd1685a7877e240e8fbf1
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2011-01-16 12:43:57 +0100

    removed UPDATING message in tsvup (showing only added elements)

--- pooch.go
+++ pooch.go
@@ -305,7 +305,7 @@ func CmdTsvUpdate(argv []string) {
 			
 			entry := ParseTsvFormat(line, tl, tl.GetTimezone());
 			if tl.Exists(entry.Id()) {
-				fmt.Printf("UPDATING\t%s\t%s\n", entry.Id(), entry.TriggerAt().Format("2006-01-02"))
+				//fmt.Printf("UPDATING\t%s\t%s\n", entry.Id(), entry.TriggerAt().Format("2006-01-02"))
 				tl.Update(entry, false)
 			} else {
 				fmt.Printf("ADDING\t%s\t%s\n", entry.Id(), entry.TriggerAt().Format("2006-01-02"))
